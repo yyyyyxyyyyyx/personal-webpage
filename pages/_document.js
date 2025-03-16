1fd4180d2c5022e490068915abf844a011dd72aa
yy <yyx_yyx@live.cn>
2025-03-16 16:49:37 +0800

    使用本地Raleway字体，解决字体加载被阻止的问题

--- pages/_document.js
+++ pages/_document.js
@@ -4,55 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* 优先加载Raleway字体 */}
+        {/* 使用本地Raleway字体 */}
         <link
-          rel="preload"
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap"
-          as="style"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
+          href="/fonts/raleway.css"
         />
-        {/* 备用字体加载方式 */}
-        <style dangerouslySetInnerHTML={{ __html: `
-          /* 备用字体加载 */
-          @font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-weight: 300;
-            font-display: swap;
-            src: url(https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
-          }
-          @font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-weight: 400;
-            font-display: swap;
-            src: url(https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
-          }
-          @font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-weight: 500;
-            font-display: swap;
-            src: url(https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
-          }
-          @font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-weight: 600;
-            font-display: swap;
-            src: url(https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
-          }
-          @font-face {
-            font-family: 'Raleway';
-            font-style: normal;
-            font-weight: 700;
-            font-display: swap;
-            src: url(https://fonts.gstatic.com/s/raleway/v28/1Ptug8zYS_SKggPNyC0ITw.woff2) format('woff2');
-          }
-        ` }} />
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
           rel="stylesheet"
